e32bdc80ef2cd9c8b13f152d0d180922d2ad8d34
John ZuHone <jzuhone@gmail.com>
2025-01-30 13:59:04 -0500

    language nit

--- acis_thermal_check/apps/copy_model_outputs.py
+++ acis_thermal_check/apps/copy_model_outputs.py
@@ -45,11 +45,11 @@ def main():
 
     location = Path(args.location)
     if not location.exists():
-        raise ValueError(f"Model location \"{location}\" does not exist!")
+        raise ValueError(f'Model location "{location}" does not exist!')
 
     rst_file = location / "index.rst"
     if not rst_file.exists():
-        raise ValueError(f"Model index file \"{rst_file}\" does not exist!")
+        raise ValueError(f'Model index file "{rst_file}" does not exist!')
 
     with open(rst_file, "r") as f:
         lines = f.readlines()
@@ -63,32 +63,34 @@ def main():
             break
 
     if not found_model_type:
-        raise ValueError(f"Could not determine model from files in \"{location}\"!")
+        raise ValueError(f'Could not determine model from files in "{location}"!')
 
     if model not in model_choices:
-        raise ValueError(f"Model \"{model}\" is not a valid model! Choices are {model_choices}")
+        raise ValueError(
+            f'Model "{model}" is not a valid model! Choices are {model_choices}'
+        )
 
-    print(f"Files to be copied are from a \"{model}\" model run.")
+    print(f'Files to be copied are from a "{model}" model run.')
 
     model_str = "FP" if model == "acisfp" else model.upper()
 
     copy_path = (
-        base_path
-        / f"{model_str}_thermPredic"
-        / load_week
-        / f"ofls{load_letter}"
+        base_path / f"{model_str}_thermPredic" / load_week / f"ofls{load_letter}"
     )
     if not copy_path.exists() and not args.dry_run:
         copy_path.mkdir(parents=True)
     if args.dry_run:
-        prefix = "Would copy"
+        prefix = "Would have copied"
     else:
-        prefix = "Copied contents of"
+        prefix = "Copied"
         try:
             shutil.copytree(location, copy_path, dirs_exist_ok=args.overwrite)
         except FileExistsError:
-            raise IOError(f"Files already exist in {copy_path} and --overwrite is not specified!")
-    print(f"{prefix} {location} to {copy_path}.")
-        
+            raise IOError(
+                f"Files already exist in {copy_path} and --overwrite is not specified!"
+            )
+    print(f"{prefix} contents of {location} to {copy_path}.")
+
+
 if __name__ == "__main__":
     main()
